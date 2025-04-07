5423a806ca69824fa045fb273bfbe9a1a13d2a49
Konrad Michalik <km@move-elevator.de>
2025-04-07 16:10:53 +0200

    fix: merge multiple indicator configuration

--- Classes/Configuration/Handler.php
+++ Classes/Configuration/Handler.php
@@ -10,6 +10,12 @@ use KonradMichalik\Typo3EnvironmentIndicator\Configuration\Trigger\TriggerInterf
 
 class Handler
 {
+    /**
+    * Adds a new indicator/trigger configuration.
+    *
+    * @param TriggerInterface[] $triggers
+    * @param IndicatorInterface[] $indicators
+    */
     public static function addIndicator(array $triggers = [], array $indicators = []): void
     {
         $configuration = [
@@ -17,22 +23,21 @@ class Handler
             'indicators' => [],
         ];
         foreach ($triggers as $trigger) {
-            if (!$trigger instanceof TriggerInterface) {
-                throw new \InvalidArgumentException('Trigger must implement TriggerInterface', 9746359720);
-            }
             $configuration['triggers'][] = $trigger;
         }
 
         foreach ($indicators as $indicator) {
-            if (!$indicator instanceof IndicatorInterface) {
-                throw new \InvalidArgumentException('Indicator must implement IndicatorInterface', 6425216089);
-            }
             $configuration['indicators'][] = $indicator;
         }
 
         $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['configuration'][] = $configuration;
     }
 
+    /**
+    * Resolves the current indicators based on the registered configurations and the checked triggers.
+    *
+    * @return array Current indicators
+    */
     public static function resolveIndicators(): array
     {
         if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['current'])) {
@@ -41,23 +46,20 @@ class Handler
 
         foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['configuration'] as $configuration) {
             foreach ($configuration['triggers'] as $trigger) {
-                /* @var TriggerInterface $trigger */
-                if (!$trigger instanceof TriggerInterface) {
-                    throw new \InvalidArgumentException('Trigger must implement TriggerInterface', 9746359720);
-                }
-
                 if (!$trigger->check()) {
                     continue 2;
                 }
             }
 
             foreach ($configuration['indicators'] as $indicator) {
-                if (!$indicator instanceof IndicatorInterface) {
-                    throw new \InvalidArgumentException('Indicator must implement IndicatorInterface', 6425216089);
+                if (isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['current'][$indicator::class])) {
+                    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['current'][$indicator::class] = array_merge_recursive(
+                        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['current'][$indicator::class],
+                        $indicator->getConfiguration()
+                    );
+                } else {
+                    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['current'][$indicator::class] = $indicator->getConfiguration();
                 }
-
-                /* @var IndicatorInterface $indicator */
-                $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][Configuration::EXT_KEY]['current'][$indicator::class] = $indicator->getConfiguration();
             }
         }
 
